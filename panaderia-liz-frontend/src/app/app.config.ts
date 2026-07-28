6f07544473ea7116be219814f6739545c105b6b0
Jorge Abraham Cueto Elias <A19106487@idat.pe>
2026-07-27 20:13:40 -0500

    feat: Configurar manifest.webmanifest para PWA

--- panaderia-liz-frontend/src/app/app.config.ts
+++ panaderia-liz-frontend/src/app/app.config.ts
@@ -1,14 +1,19 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { errorInterceptor } from './services/error.interceptor';
-import { authInterceptor } from './services/auth.interceptor'; // <--- Importa tu interceptor de auth
+import { authInterceptor } from './services/auth.interceptor';
+import { provideServiceWorker } from '@angular/service-worker'; // <--- Importa tu interceptor de auth
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])) // <--- Agrégalo aquí primero
-  ]
+    provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }), // <--- Agrégalo aquí primero
+  ],
 };
